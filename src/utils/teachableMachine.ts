40f2fe59ad9af14ee1e546870c9a5c2725fdc977
Rahmat Ramadhani <ramadhanirahmat47@gmail.com>
2026-09-12 17:11:49 +0700

    chore: update default Teachable Machine model URL
    Switch the default model to the latest version and clean up redundant URLs in documentation.

--- src/utils/teachableMachine.ts
+++ src/utils/teachableMachine.ts
@@ -38,7 +38,7 @@ export interface TmWebcam {
 
 // Default verified example Teachable Machine model URL for Rock-Paper-Scissors
 // User can easily replace this with their own Teachable Machine link!
-export const DEFAULT_MODEL_URL = 'https://teachablemachine.withgoogle.com/models/I517u8V2z/';
+export const DEFAULT_MODEL_URL = 'https://teachablemachine.withgoogle.com/models/mtEKos-_I/';
 
 /**
  * Normalizes labels from different user models (Indonesian & English variants)
